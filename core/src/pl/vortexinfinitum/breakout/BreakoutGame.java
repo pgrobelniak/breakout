b3336ffbf9456fd79808dda4965f57fdbcdaa002
Pawel Grobelniak <pawel.grobelniak@gmail.com>
2022-07-24 12:29:48 +0200

    easing

--- core/src/pl/vortexinfinitum/breakout/BreakoutGame.java
+++ core/src/pl/vortexinfinitum/breakout/BreakoutGame.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
+import pl.vortexinfinitum.game.utils.Easing;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,135 +16,150 @@ import java.util.Random;
 
 public class BreakoutGame extends ApplicationAdapter {
 
-	int PADDLE_SPEED = 450;
+    int PADDLE_SPEED = 450;
 
-	int PADDLE_WIDTH = 150;
+    int PADDLE_WIDTH = 150;
 
-	int BLOCK_HEIGHT = 30;
+    int BLOCK_HEIGHT = 30;
 
-	int BALL_SIZE = 30;
+    int BALL_SIZE = 30;
 
-	int BALL_SPEED = 400;
+    int BALL_SPEED = 400;
 
-	int ROWS = 4;
+    int ROWS = 4;
 
-	int COLS = 10;
+    int COLS = 10;
 
-	int EMPTY_ROWS = 3;
+    int EMPTY_ROWS = 3;
 
-	Random random = new Random();
+    int INTRO_DURATION = 2000;
 
-	SpriteBatch batch;
+    long introStartTime;
 
-	Texture img;
+    Random random = new Random();
 
-	Rectangle paddleRect;
+    SpriteBatch batch;
 
-	Rectangle ballRect;
+    Texture img;
 
-	Vector2 ballVel;
+    Rectangle paddleRect;
 
-	boolean gameRunning;
+    Rectangle ballRect;
 
-	List<Rectangle> blocks = new ArrayList<>();
+    Vector2 ballVel;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		resetGame();
-	}
+    List<Rectangle> blocks = new ArrayList<>();
 
-	@Override
-	public void render () {
-		updatePaddle();
-		updateBall();
-		draw();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+        resetGame();
+    }
 
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void render() {
+        updatePaddle();
+        if (gameInProgress()) {
+            updateBall();
+        }
+        draw();
+    }
 
-	private void resetGame() {
-		gameRunning = false;
-		paddleRect = new Rectangle((Gdx.graphics.getWidth() - PADDLE_WIDTH) / 2f, 0, PADDLE_WIDTH, BLOCK_HEIGHT);
-		ballRect = new Rectangle((Gdx.graphics.getWidth() - BALL_SIZE) / 2f, BLOCK_HEIGHT, BALL_SIZE, BALL_SIZE);
-		ballVel = new Vector2(random.nextFloat() * 2 - 1, 1);
-		blocks.clear();
-		int blockWidth = Gdx.graphics.getWidth() / COLS;
-		for (int r = 0; r < ROWS; r++) {
-			for (int c = 0; c < COLS; c++) {
-				blocks.add(new Rectangle(c * blockWidth + 1, Gdx.graphics.getHeight() - EMPTY_ROWS * BLOCK_HEIGHT - r *
-						BLOCK_HEIGHT - BLOCK_HEIGHT - 1, blockWidth - 2, BLOCK_HEIGHT - 2));
-			}
-		}
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 
-	private void updatePaddle() {
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			paddleRect.x -= Gdx.graphics.getDeltaTime() * PADDLE_SPEED;
-			gameRunning = true;
-		}
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			paddleRect.x += Gdx.graphics.getDeltaTime() * PADDLE_SPEED;
-			gameRunning = true;
-		}
-	}
+    private void resetGame() {
+        introStartTime = System.currentTimeMillis();
+        paddleRect = new Rectangle((Gdx.graphics.getWidth() - PADDLE_WIDTH) / 2f, 0, PADDLE_WIDTH, BLOCK_HEIGHT);
+        ballRect = new Rectangle((Gdx.graphics.getWidth() - BALL_SIZE) / 2f, BLOCK_HEIGHT * 4, BALL_SIZE, BALL_SIZE);
+        ballVel = new Vector2(random.nextFloat() * 2 - 1, 1);
+        blocks.clear();
+        int blockWidth = Gdx.graphics.getWidth() / COLS;
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                blocks.add(new Rectangle(c * blockWidth + 1, Gdx.graphics.getHeight() - EMPTY_ROWS * BLOCK_HEIGHT - r *
+                        BLOCK_HEIGHT - BLOCK_HEIGHT - 1, blockWidth - 2, BLOCK_HEIGHT - 2));
+            }
+        }
+    }
 
-	private void updateBall() {
-		if (gameRunning) {
-			ballRect.x += ballVel.x * Gdx.graphics.getDeltaTime() * BALL_SPEED;
-			ballRect.y += ballVel.y * Gdx.graphics.getDeltaTime() * BALL_SPEED;
-			if (ballRect.overlaps(paddleRect)) {
-				ballVel.x = (ballRect.x + BALL_SIZE / 2f - paddleRect.x - PADDLE_WIDTH / 2f) / (PADDLE_WIDTH / 2f);
-				ballVel.y = -ballVel.y;
-				ballRect.y = paddleRect.y + BLOCK_HEIGHT;
-			} else if (ballRect.y < 0) {
-				resetGame();
-			} else {
-				List<Rectangle> bouncedBlocks = new ArrayList<>();
-				for (Rectangle block : blocks) {
-					if (block.overlaps(ballRect)) {
-						bouncedBlocks.add(block);
-					}
-				}
-				if (bouncedBlocks.size() > 0) {
-					for (Rectangle block : bouncedBlocks) {
-						blocks.remove(block);
-					}
-					if (blocks.size() == 0) {
-						resetGame();
-					} else {
-						ballVel.y = -ballVel.y;
-					}
-				}
-				if (ballRect.y + BALL_SIZE > Gdx.graphics.getHeight()) {
-					ballRect.y = Gdx.graphics.getHeight() - BALL_SIZE;
-					ballVel.y = -ballVel.y;
-				}
-				if (ballRect.x < 0) {
-					ballRect.x = 0;
-					ballVel.x = -ballVel.x;
-				}
-				if (ballRect.x + BALL_SIZE > Gdx.graphics.getWidth()) {
-					ballRect.x = Gdx.graphics.getWidth() - BALL_SIZE;
-					ballVel.x = -ballVel.x;
-				}
-			}
-		}
-	}
+    private void updatePaddle() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            paddleRect.x -= Gdx.graphics.getDeltaTime() * PADDLE_SPEED;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            paddleRect.x += Gdx.graphics.getDeltaTime() * PADDLE_SPEED;
+        }
+    }
 
-	private void draw() {
-		ScreenUtils.clear(0, 0, 1, 1);
-		batch.begin();
-		batch.draw(img, paddleRect.x, paddleRect.y, paddleRect.width, paddleRect.height);
-		batch.draw(img, ballRect.x, ballRect.y, ballRect.width, ballRect.height);
-		for (Rectangle rectangle : blocks) {
-			batch.draw(img, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-		}
-		batch.end();
-	}
+    private void updateBall() {
+        ballRect.x += ballVel.x * Gdx.graphics.getDeltaTime() * BALL_SPEED;
+        ballRect.y += ballVel.y * Gdx.graphics.getDeltaTime() * BALL_SPEED;
+        if (ballRect.overlaps(paddleRect)) {
+            ballVel.x = (ballRect.x + BALL_SIZE / 2f - paddleRect.x - PADDLE_WIDTH / 2f) / (PADDLE_WIDTH / 2f);
+            ballVel.y = -ballVel.y;
+            ballRect.y = paddleRect.y + BLOCK_HEIGHT;
+        } else if (ballRect.y < 0) {
+            resetGame();
+        } else {
+            List<Rectangle> bouncedBlocks = new ArrayList<>();
+            for (Rectangle block : blocks) {
+                if (block.overlaps(ballRect)) {
+                    bouncedBlocks.add(block);
+                }
+            }
+            if (bouncedBlocks.size() > 0) {
+                for (Rectangle block : bouncedBlocks) {
+                    blocks.remove(block);
+                }
+                if (blocks.size() == 0) {
+                    resetGame();
+                } else {
+                    ballVel.y = -ballVel.y;
+                }
+            }
+            if (ballRect.y + BALL_SIZE > Gdx.graphics.getHeight()) {
+                ballRect.y = Gdx.graphics.getHeight() - BALL_SIZE;
+                ballVel.y = -ballVel.y;
+            }
+            if (ballRect.x < 0) {
+                ballRect.x = 0;
+                ballVel.x = -ballVel.x;
+            }
+            if (ballRect.x + BALL_SIZE > Gdx.graphics.getWidth()) {
+                ballRect.x = Gdx.graphics.getWidth() - BALL_SIZE;
+                ballVel.x = -ballVel.x;
+            }
+        }
+    }
+
+    private void draw() {
+        ScreenUtils.clear(0, 0, 1, 1);
+        batch.begin();
+        for (Rectangle rectangle : blocks) {
+            batch.draw(img, rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+        }
+        batch.draw(img, paddleRect.x, paddleRect.y, paddleRect.width, paddleRect.height);
+        Rectangle rect;
+        if (gameInProgress()) {
+            rect = ballRect;
+        } else {
+            rect = new Rectangle();
+            float percent = (System.currentTimeMillis() - introStartTime) / (float) INTRO_DURATION;
+            rect.x = Easing.easeOutBounce(0, ballRect.x, percent);
+            rect.y = Easing.easeOutBounce(0, ballRect.y, percent);
+            rect.width = Easing.easeOutBounce(Gdx.graphics.getWidth(), ballRect.width, percent);
+            rect.height = Easing.easeOutBounce(Gdx.graphics.getHeight(), ballRect.height, percent);
+        }
+        batch.draw(img, rect.x, rect.y, rect.width, rect.height);
+        batch.end();
+    }
+
+    private boolean gameInProgress() {
+        return System.currentTimeMillis() - introStartTime > INTRO_DURATION;
+    }
 }
---
+++ core/src/pl/vortexinfinitum/game/utils/Easing.java
@@ -0,0 +1,42 @@
+package pl.vortexinfinitum.game.utils;
+
+//https://easings.net/
+public class Easing {
+
+    public static float easeOutElastic(float from, float to, float percent) {
+        return from + (to - from) * easeOutElastic(percent);
+    }
+
+    public static float easeInOutElastic(float from, float to, float percent) {
+        return from + (to - from) * easeInOutElastic(percent);
+    }
+
+    public static float easeOutBounce(float from, float to, float percent) {
+        return from + (to - from) * easeOutBounce(percent);
+    }
+
+    public static float easeOutElastic(Float x) {
+        float c4 = (2 * (float) Math.PI) / 3;
+        return (float) (x == 0 ? 0 : x == 1 ? 1 : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1);
+    }
+
+
+    public static float easeInOutElastic(Float x) {
+        float c5 = (2 * (float) Math.PI) / 4.5f;
+        return (float) (x == 0 ? 0 : x == 1 ? 1 : x < 0.5 ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2 : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1);
+    }
+
+    public static float easeOutBounce(Float x) {
+        float n1 = 7.5625f;
+        float d1 = 2.75f;
+        if (x < 1 / d1) {
+            return n1 * x * x;
+        } else if (x < 2 / d1) {
+            return n1 * (x -= 1.5f / d1) * x + 0.75f;
+        } else if (x < 2.5 / d1) {
+            return n1 * (x -= 2.25f / d1) * x + 0.9375f;
+        } else {
+            return n1 * (x -= 2.625f / d1) * x + 0.984375f;
+        }
+    }
+}
